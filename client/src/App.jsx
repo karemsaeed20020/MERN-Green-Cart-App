383f2cd11d22f5a855da880dd76903fe0245e1e6
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-04-08 12:57:40 +0200

    Create BestSeller and Product Card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
+import {Toaster} from 'react-hot-toast'
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <>
       {isSellerPath ? null : <Navbar /> }  
+      <Toaster />
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
