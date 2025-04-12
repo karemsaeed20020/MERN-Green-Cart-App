13ab1e497af96368ddc1efaf0295e28c86a06cf4
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-04-12 09:31:47 +0200

    Add Schema of Address and Get, Add Address

---
+++ server/controllers/addressController.js
@@ -0,0 +1,25 @@
+import Address from "../models/Address.js";
+
+// Add Address : /api/address/add
+export const addAddress = async (req, res) => {
+    try {
+        const {userId, address} = req.body;
+        await Address.create({...address,  userId})
+        res.status(201).json({success: true, message: "Address Added Successfully"})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+// Get Address : /api/address/get
+export const getAddress = async (req, res) => {
+    try {   
+        const {userId} = req.body;
+        const addresses = await Address.find({userId});
+        res.status(201).json({success: true, addresses})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
